91513fe0316f79bea4853c82a253aef77630051a
Debasish Paul <debasish@agile-labs.com>
2025-11-07 12:19:32 +0530

    Flutter Package: added  arm_encrypt_secret-final commit

--- Axpert Samples/Flutter_Packages/arm_encrypt_secret/lib/src/ax_exceptions.dart
+++
@@ -1,8 +0,0 @@
-class AesEncryptionException implements Exception {
-  final String message;
-  final dynamic cause;
-  AesEncryptionException(this.message, [this.cause]);
-  @override
-  String toString() =>
-      'AesEncryptionException: $message${cause != null ? " | Cause: $cause" : ""}';
-}
